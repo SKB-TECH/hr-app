be83b90b6f2565c9cc27f9bb92a34313ecdb4a04
Freddy Bijanja <freddybijanja31@gmail.com>
2026-05-15 08:11:54 +0200

    feature/add about ceo message section

--- app/[locale]/(pages)/about/page.tsx
+++ app/[locale]/(pages)/about/page.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import CEOMessage from "@/components/CEOMessage";
 
 const AboutUsPage = () => {
   return (
     <div className='min-h-screen mx-auto'>
       <h2 className='text-4xl font-black text-center'>About Us</h2>
+      <CEOMessage />
     </div>
   );
 };
