e0cc4accfc5378b2b837ba47381cc16fe24471a9
Freddy Bijanja <freddybijanja31@gmail.com>
2026-05-19 19:36:03 +0200

    fix axios issues installation

--- services/contact.service.ts
+++ services/contact.service.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import api from "../lib/axios";
 import type { ContactFormData } from "../types/contact";
 
@@ -7,10 +6,8 @@ export const sendContactForm = async (formData: ContactFormData) => {
     const response = await api.post("/contact", formData);
     return response.data;
   } catch (error) {
-    if (axios.isAxiosError(error)) {
-      throw new Error(
-        error.response?.data.message || "Unable to send your message.",
-      );
+    if (error instanceof Error) {
+      throw new Error(error.message || "Unable to send your message.");
     }
     throw new Error("An unexpected error occurred. Please try again later.");
   }
