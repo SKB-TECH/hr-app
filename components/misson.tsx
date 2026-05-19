3b42b234d49aeabfb02dd8c367b42e5e62389325
josue <izeremanzijosue2@gmail.com>
2026-05-19 08:16:52 +0200

    Add mission section component

--- components/misson.tsx
+++ components/misson.tsx
@@ -6,21 +6,23 @@ import Image from "next/image";
 
 export const Misson = () => {
   return (
-    <div className="items-center ">
-      <div className="flex flex-col lg:flex-row items-center mx-auto">
-        <div className="w-full lg:w-1/2 lg:px-18 px-6 py-16 md:px-12 max-w-7xl mx-auto">
-          <ReusableTittle
-            firstTittle="misson"
-            secondTittle="We’ve managed over 2.5 million candidates"
-            text="Lorem ipsum dolor sit amet consectetur. Turpis sed pulvinar sed blandit rhoncus tellus senectus at quis. Mi at fermentum imperdiet velit magna a aliquam. Faucibus et quam ac elit placerat tristique vulputate. Elit sit varius condimentum tempor vel commodo malesuada."
-          />
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-10">
+      <div className="flex flex-col lg:flex-row min-h-[500px] md:min-h-[600px]">
+        <div className="w-full lg:w-1/2 flex items-center justify-center py-10 md:py-0">
+          <div className=" md:text-left">
+            <ReusableTittle
+              firstTittle="misson"
+              secondTittle="We’ve managed over 2.5 million candidates"
+              text="Lorem ipsum dolor sit amet consectetur. Turpis sed pulvinar sed blandit rhoncus tellus senectus at quis. Mi at fermentum imperdiet velit magna a aliquam. Faucibus et quam ac elit placerat tristique vulputate. Elit sit varius condimentum tempor vel commodo malesuada."
+            />
+            <ReusableButton icon={<ArrowRight />} />
+          </div>
         </div>
-
-        <div className="w-full lg:w-1/2 h-full md:h-full  ">
+        <div className="w-full lg:w-1/2 h-[300px] sm:h-[400px] md:h-[600px] lg:h-auto">
           <img
             src="/images/image.png"
             alt="image"
-            className="w-full h-full  md:h-full object-cover"
+            className="w-full h-full object-cover"
           />
         </div>
       </div>
