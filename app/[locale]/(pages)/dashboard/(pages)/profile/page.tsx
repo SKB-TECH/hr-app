a70995cc8b81e0ae436e26a0b8dfffa489f66e50
Racine14Creator <bisimwagrace596@gmail.com>
2026-05-27 17:22:45 +0200

    Fixing the profile page

--- app/[locale]/(pages)/dashboard/(pages)/profile/page.tsx
+++ app/[locale]/(pages)/dashboard/(pages)/profile/page.tsx
@@ -1,11 +1,11 @@
 export default function ProfilePage() {
-  reutrn(
+  return (
     <main className='flex-1'>
       <h1>Profile Page</h1>
       <p>
         You will implement the user profile page here, where users can view and
         edit their personal information.
       </p>
-    </main>,
+    </main>
   );
 }
