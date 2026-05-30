73b0d26a2cdccbfa0273d0b760f1bd1d5e94a6fd
josue <izeremanzijosue2@gmail.com>
2026-05-30 12:25:58 +0200

    added reusable herosection

--- data/data.ts
+++ data/data.ts
@@ -1,1 +1,8 @@
 // use this file for data that is used across the app, such as arrays of objects, constants, etc.
+export const popularTags = [
+  "UI Designer",
+  "UX Researcher",
+  "Frontend Developer",
+  "Backend Developer",
+  "Product Manager"
+];
