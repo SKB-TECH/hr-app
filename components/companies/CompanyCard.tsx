bcd3e6332a70a77c69abecd8e41e1fb10a55da14
Leon <irakaramale@gmail.com>
2026-05-31 17:57:11 +0300

    Make CompanyCard responsive and add location display

--- components/companies/CompanyCard.tsx
+++ components/companies/CompanyCard.tsx
@@ -7,19 +7,30 @@ interface CompanyCardProps {
 
 export function CompanyCard({ company }: CompanyCardProps) {
   return (
-    <div className="border border-brand-light-neutral space-y-1 p-5 hover:shadow-sm transition-shadow cursor-pointer bg-white">
+    <div className="border border-brand-light-neutral rounded-sm space-y-1 p-5 hover:shadow-sm transition-shadow cursor-pointer bg-white">
       <div className="flex justify-between items-start ">
         <div className="flex flex-col gap-2">
-          <Image src={company.src} alt={company.name} width={80} height={80} />
-          <h2 className="text-[24px] font-bold text-neutral-100 my-2 block">
-            {company.name}
-          </h2>
+          <Image
+            src={company.src}
+            alt={company.name}
+            width={80}
+            height={80}
+            className="max-sm:w-[48px] max-sm:h-[48px]"
+          />
+          <div className="flex flex-col ">
+            <h2 className="max-md:text-[20px] text-[24px] font-semibold md:font-bold text-neutral-100 md:my-2 block">
+              {company.name}
+            </h2>
+            <p className="max-md:block hidden text-[16px]  md:font-bold text-neutral-100 mb-2 ">
+              {company.location}
+            </p>
+          </div>
         </div>
-        <span className="bg-accent-light-brand text-brand px-3 text-sm py-0.5 rounded-full">
+        <span className="bg-accent-light-brand text-brand px-3 text-sm py-1 ">
           {company.availableJobs} Jobs
         </span>
       </div>
-      <p className="text-neutral-80 text-[18px] mb-3 line-clamp-4">
+      <p className="text-neutral-80 text-[16px] mb-3 line-clamp-2 md:line-clamp-4">
         {company.description}
       </p>
       <div className="flex flex-wrap gap-2">
