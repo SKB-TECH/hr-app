24591ed04545b2fd4c760d365b24fe616de41ed7
Racine14Creator <bisimwagrace596@gmail.com>
2026-05-31 12:11:16 +0200

    Implementing the Section component with some dummy data

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,8 +1,15 @@
 
 import HeroSection from "@/components/landing/hero/HeroSection";
+import SectionComponent from "@/components/common/section/Section";
 
 const Page = () => {
-  return <HeroSection />;
+  return (
+      <>
+        <HeroSection />
+        {/*  Companies we helped grow  */}
+          <SectionComponent/>
+      </>
+  );
 };
 
 export default Page;
---
+++ components/common/section/Section.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import {ArrowRight, FolderArchive} from "lucide-react";
+
+export default function SectionComponent(){
+    return (
+        <div className="bg-white px-8">
+            <section className="max-w-6xl mx-auto py-8 px-4 md:px-0">
+            <div className="flex flex-col md:flex-row justify-between items-center">
+                <h1 className="font-bold text-3xl flex flex-row gap-2">
+                    <span className="text-black dark:text-white">Explore by</span>
+                    <span className="text-brand">category</span>
+                </h1>
+                <Link href="#" className="flex flex-row text-brand text-xs font-bold items-center gap-1">
+                    <span className="">Show all jobs</span>
+                    <ArrowRight size={12}/>
+                </Link>
+            </div>
+
+            {/*    Elements   */}
+            <div className="py-5 md:py-12 grid grid-cols-1 md:grid-cols-4 gap-5">
+                {[1,2,3,4,5, 6, 7, 8].map((item, i) => (
+                    <div key={i} className="border cursor-pointer group hover:bg-brand p-5 md:p-6 flex flex-col gap-3 justify-between">
+                        <FolderArchive size={32} className="mb-3 text-brand group-hover:text-white"/>
+                        <div className="flex flex-col gap-3">
+                            <h2 className="font-bold text-black group-hover:text-white text-2xl">Design</h2>
+                            <Link href="#" className="flex flex-row justify-between text-foreground group-hover:text-white items-center gap-2">
+                                <span className=" text-sm">235 jobs available</span>
+                                <ArrowRight size={15} className="font-normal"/>
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
+        </div>
+    )
+}
