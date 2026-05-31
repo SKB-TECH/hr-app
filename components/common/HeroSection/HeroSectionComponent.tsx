349e31aea88d9a114c4614b8b1c4d22807791274
Racine14Creator <bisimwagrace596@gmail.com>
2026-05-31 21:27:17 +0200

    Landing page... The hero section!

--- components/common/HeroSection/HeroSectionComponent.tsx
+++ components/common/HeroSection/HeroSectionComponent.tsx
@@ -2,11 +2,17 @@ export default function HeroSectionComponent(){
     return (
         <div className="">
             <div className="bg-pattern position-relative min-h-[500px] max-w-6xl mx-auto">
-                <div className="max-w-4xl min-h-[100%] bg-indigo-500 flex items-center">
-                    <div className="">
-                        <h1 className="font-heading text-3xl font-semibold">Discover <br/> more than
+                <div className="max-w-4xl py-32 flex flex-col justify-center">
+                    <div className="max-w-1/2">
+                        <h1 className="font-heading text-3xl font-semibold flex flex-col gap-2">Discover <br/> more than
                             <span className="text-primary">5000+ Jobs</span>
                         </h1>
+                    </div>
+
+                    <div className="max-w-5/12">
+                        <span className="">
+                            Great platform for the job seeker that searching for new career heights and passionate about startups.
+                        </span>
                     </div>
                 </div>
             </div>
