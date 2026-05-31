1dda269db624bd340d9237892bc3be19e9738ce0
Leon <irakaramale@gmail.com>
2026-05-31 19:19:27 +0300

    Restore data.ts from merge state

---
+++ data/data.ts
@@ -0,0 +1,8 @@
+// use this file for data that is used across the app, such as arrays of objects, constants, etc.
+
+import {NavigationTypes} from "@/types/navigationTypes";
+
+export const navigations: (NavigationTypes)[] = [
+    {id: "1", name: "Find Jobs", path: "/jobs"},
+    {id: "2", name: "Browser Companies", path: "/companies"},
+]
