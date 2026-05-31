b4b2d1277c59bf5e08bcbb2f5dc581242638de59
Leon <irakaramale@gmail.com>
2026-05-31 17:58:33 +0300

    Finalize Search page layout

--- app/[locale]/(pages)/companies/search/page.tsx
+++ app/[locale]/(pages)/companies/search/page.tsx
@@ -1,21 +1,16 @@
-import { companySizes, industries } from "../../../../../data/companyPageData";
+import { mockSidebarCompanyFilters } from "../../../../../data/companyPageData";
 import { SidebarFilters } from "@/components/companies/SidebarFilters";
 import CompanyList from "@/components/companies/CompanyList";
-import MobileFilterButton from "@/components/companies/MobileFilterButton";
-import MobileFilters from "@/components/companies/MobileFilters";
+import SharedDisplayMobileFilter from "@/components/companies/SharedDisplayMobileFilter";
 
 function SearchCompaniesPage() {
   return (
     <section className="min-h-screen relative">
-      {/* <MobileFilters /> */}
-      <MobileFilterButton />
-      <div className="px-6 py-16 pt-4 md:px-12 max-w-7xl mx-auto">
+      <SharedDisplayMobileFilter DataToFilter={mockSidebarCompanyFilters} />
+      <div className="px-4 py-16 pt-4 md:px-12 max-w-7xl mx-auto">
         <div className="flex flex-col md:flex-row gap-8">
           <div className="hidden md:block">
-            <SidebarFilters
-              industries={industries}
-              companySizes={companySizes}
-            />
+            <SidebarFilters sidebarFilterData={mockSidebarCompanyFilters} />
           </div>
 
           <CompanyList />
