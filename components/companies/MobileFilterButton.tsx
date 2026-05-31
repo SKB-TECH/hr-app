04886722a6a76e7163d9fb82592ce17a0111302c
Leon <irakaramale@gmail.com>
2026-05-31 17:57:13 +0300

    Update mobile filter components for state handling and shared data

--- components/companies/MobileFilterButton.tsx
+++ components/companies/MobileFilterButton.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
-import { SidebarFilters } from "./SidebarFilters";
-import { companySizes, industries } from "@/data/companyPageData";
 
-function MobileFilterButton() {
+function MobileFilterButton({
+  setShowMobileFilters,
+}: {
+  setShowMobileFilters: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   return (
-    <div className="flex items-center justify-center md:hidden mb-4 gap-2 font-clash font-medium text-neutral-100 text-[16px] p-4 border border-brand-light-neutral">
+    <div
+      onClick={() => setShowMobileFilters((prev) => !prev)}
+      className="flex items-center justify-center md:hidden mb-4 gap-2 font-clash font-medium text-neutral-100 text-[16px] p-4 border border-brand-light-neutral"
+    >
       <Image src="/filter.png" alt="filter icon" width={20} height={20} />
       <p>More Filters</p>
-      {/* <SidebarFilters industries={industries} companySizes={companySizes} /> */}
     </div>
   );
 }
--- components/companies/MobileFilters.tsx
+++ components/companies/MobileFilters.tsx
@@ -1,37 +1,40 @@
 import { Button } from "../ui/button";
+import { SharedFilterSection } from "./SharedFilterSection";
+import { X } from "lucide-react";
+import { SidebarFilterCompany } from "@/types/types";
 
-function MobileFilters() {
-  const title = "Type of Employment";
-  const options = [
-    { name: "Remote", count: 24 },
-    { name: "Onsite", count: 10 },
-    { name: "Hybrid", count: 7 },
-  ];
-
+function MobileFilters({
+  setShowMobileFilters,
+  DataToFilter,
+}: {
+  setShowMobileFilters: React.Dispatch<React.SetStateAction<boolean>>;
+  DataToFilter: SidebarFilterCompany[];
+}) {
   return (
     <div className="max-md:flex fixed hidden inset-0 bg-neutral-100/30 z-50  items-center justify-center">
-      <div className="flex flex-col mb-8 bg-white h-[80%] w-[90%]  p-6">
-        <h1 className="font-semibold text-neutral-100 font-clash text-[18px] mb-4">
-          More Filters
-        </h1>
-        <h3 className="font-semibold text-neutral-100 font-clash text-[14px] mb-4">
-          {title}
-        </h3>
-        <div className="space-y-4">
-          {options.map((option) => (
-            <div
-              key={option.name}
-              className="flex items-center text-[16px] justify-start gap-3"
-            >
-              <div className="w-4 h-4 rounded-sm border bg-white/5" />
-              <div className="flex items-center gap-1">
-                <span className="text-slate-600">{option.name}</span>
-                <span className="text-slate-400">({option.count})</span>
-              </div>
-            </div>
-          ))}
+      <div className="flex flex-col  bg-white  h-[95vh] w-[90%]  p-6 overflow-y-auto">
+        <div className="flex justify-between items-center mb-4 text-neutral-100">
+          <h3 className="font-semibold  font-clash text-[20px]">
+            More Filters
+          </h3>
+          <X
+            size={24}
+            className="cursor-pointer"
+            onClick={() => setShowMobileFilters(false)}
+          />
         </div>
-        <Button className="rounded-none bg-brand w-full  mt-auto">Apply</Button>
+
+        {DataToFilter.map((filter) => (
+          <SharedFilterSection
+            key={filter.title}
+            title={filter.title}
+            options={filter.options}
+          />
+        ))}
+
+        <Button className="rounded-none bg-brand w-full py-1  mt-auto">
+          Apply
+        </Button>
       </div>
     </div>
   );
