52619567aaba721ac5247df83de34c24ee88f242
Leon <irakaramale@gmail.com>
2026-06-09 18:48:22 +0300

    Sync next.config.ts with develop to fix merge conflicts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-import createNextIntlPlugin from "next-intl/plugin";
-import type { Configuration } from "webpack";
+import createNextIntlPlugin from 'next-intl/plugin';
+import type { Configuration } from 'webpack';
 
-const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
+const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 const nextConfig = {
   webpack: (config: Configuration) => {
     config.watchOptions = {
       poll: 1000,
       aggregateTimeout: 300,
-    };
-    return config;
+    }
+    return config
   },
 };
 
