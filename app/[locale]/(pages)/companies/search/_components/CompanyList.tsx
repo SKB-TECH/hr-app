1dc54c207197225d163ca247d7e7e33506cd321a
Leon <irakaramale@gmail.com>
2026-06-10 14:42:32 +0300

    add reusable header & paginations

--- app/[locale]/(pages)/companies/search/_components/CompanyList.tsx
+++ app/[locale]/(pages)/companies/search/_components/CompanyList.tsx
@@ -2,13 +2,26 @@
 import { companies, sortOptions } from "@/data/companyPageData";
 import { SharedListingHeader } from "@/components/shared/listing/SharedListingHeader";
 import { CompanyCard } from "./CompanyCard";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import Pagination from "../../../jobs/ReusablePagination/ReusablePagination";
 
 function CompanyList() {
+  const listRef = useRef<HTMLDivElement>(null);
   const [viewGrid, setViewGrid] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const companiesToRender = 5;
+  const totalPages = Math.ceil(companies.length / companiesToRender);
+  const startIndex = (currentPage - 1) * companiesToRender;
+  const endIndex = startIndex + companiesToRender;
+  const paginatedCompanies = companies.slice(startIndex, endIndex);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    listRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
 
   return (
-    <main className="flex-1">
+    <main className="flex-1 ">
       <SharedListingHeader
         totalResults={companies.length}
         sortOptions={sortOptions}
@@ -18,15 +31,24 @@ function CompanyList() {
       />
 
       <div
+        ref={listRef}
+        key={currentPage}
         className={
           viewGrid === true
-            ? "grid grid-cols-1 md:grid-cols-2 gap-6 transition-all duration-300 ease-in-out"
-            : "flex flex-col gap-6 transition-all duration-300 ease-in-out"
+            ? "grid grid-cols-1 md:grid-cols-2 gap-6 transition-all duration-300 ease-in-out animate-in fade-in slide-in-from-bottom-2"
+            : "flex flex-col gap-6 transition-all duration-300 ease-in-out animate-in fade-in slide-in-from-bottom-2"
         }
       >
-        {companies.map((company) => (
+        {paginatedCompanies.map((company) => (
           <CompanyCard key={company.id} company={company} />
         ))}
+      </div>
+      <div className="flex justify-center mt-8">
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
       </div>
     </main>
   );
