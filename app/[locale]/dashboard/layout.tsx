5dde19116192e4594941c135512eb70837437bf8
Leon <irakaramale@gmail.com>
2026-06-11 13:05:58 +0300

    align dashboard paddings

--- app/[locale]/dashboard/layout.tsx
+++ app/[locale]/dashboard/layout.tsx
@@ -1,5 +1,5 @@
-import Sidebar from "./_components/Sidebar";
-import DashBoardHeaderWrapper from "./_components/DashBoardHeaderWrapper";
+import Sidebar from "../../../components/dashboard/layout/Sidebar";
+import DashBoardHeaderWrapper from "../../../components/dashboard/layout/DashBoardHeaderWrapper";
 
 export default function DashboardLayout({
   children,
@@ -11,9 +11,9 @@ export default function DashboardLayout({
       <div className="hidden lg:block w-1/6 ">
         <Sidebar />
       </div>
-      <div className="w-5/6 max-lg:w-full  ">
+      <div className="w-5/6 max-lg:w-full   ">
         <DashBoardHeaderWrapper />
-        {children}
+        <div className="p-4 px-6">{children}</div>
       </div>
     </div>
   );
